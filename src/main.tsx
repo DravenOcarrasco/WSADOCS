df61b439bcbc8d89591d71602b770b55c108cc38
Marcos Brendon De Paula <166177842+DravenOcarrasco@users.noreply.github.com>
2024-10-22 18:38:29 -0300

    build

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter basename="/WSADOCS/"> {/* Define o basename conforme o base em vite.config.js */}
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
